3fd6dfdf9891a3e95877a05c671ce0423b2a3fb1
AD1083 <56340314+AD1083@users.noreply.github.com>
2019-11-04 14:21:46 +0000

    master

---
+++ Healthcare_Systemnow/Healthcare_Systemnow/Healthcare_System/Views/CentralDeskView.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Healthcare_System.Views;
+
+namespace Healthcare_System
+{
+    public partial class CentralDeskView : Form //, ICentralDeskView
+    {
+
+        public CentralDeskView()
+        {
+            InitializeComponent();
+            this.Load += CentralDeskView_Load;
+            btnSignOut.Click += BtnSignOut_Click;
+            btnRoom1.Click += (sender, args) => Invoke(ViewPatient);
+            btnRoom2.Click += (sender, args) => Invoke(ViewPatient);
+        }
+        //properties
+        public string FirstNameRoom1 { get { return lblFirstnameRoom1.Text; } }
+        public string LastnameRoom1 { get { return lblLastNameRoom1.Text; } }
+        public string ConditionRoom1 { get { return lblConditionRoom1.Text; } }
+        public string Room1 { get { return lblRoom1.Text; } }
+        private void BtnSignOut_Click(object sender, EventArgs e)
+        {
+            if(SignOut!=null) SignOut(this, EventArgs.Empty);
+        }
+
+        public event EventHandler SignOut;
+        public event Action ViewPatient;
+
+        private void Invoke(Action action)
+        {
+            if (action != null) action();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+
+
+        public event EventHandler StartSimulation;
+        private void CentralDeskView_Load(object sender, EventArgs e)
+        {
+            if (StartSimulation != null) StartSimulation(this, EventArgs.Empty);
+
+        }
+
+        public new void Show()
+        {
+            this.ShowDialog();
+        }
+    }
+}
---
+++ Healthcare_Systemnow/Healthcare_Systemnow/Healthcare_System/Views/ICentralDeskView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcare_System.Views
+{
+    public interface ICentralDeskView
+    {
+        event Action ViewPatient;
+        event EventHandler SignOut;
+        event EventHandler StartSimulation;
+
+        string FirstNameRoom1 { get; set; }
+        string LastnameRoom1 { get; set; }
+        string ConditionRoom1 { get; set; }
+        string Room1 { get; set; }
+        void Show();
+    }
+}
