c0ebcd60e51214e554e2b1789ab2c4d9b1f84de7
AD1083 <56340314+AD1083@users.noreply.github.com>
2019-11-07 11:56:49 +0000

    Email class
    email class for notifications

--- Healthcare_Systemnow/Healthcare_Systemnow/Healthcare_System/Models/EmailNotifications.cs
+++ Healthcare_Systemnow/Healthcare_Systemnow/Healthcare_System/Models/EmailNotifications.cs
@@ -8,7 +8,7 @@ using System.Net;
 
 namespace Healthcare_System.Models
 {
-    //https://docs.microsoft.com/en-us/dotnet/api/system.net.mail?view=netframework-4.8
+    //https://docs.microsoft.com/en-us/dotnet/api/system.net.mail?view=netframework-4.8 System.Net.Mail namespace
     class EmailNotifications
     {
         //https://docs.microsoft.com/en-us/dotnet/api/system.net.mail.smtpclient?view=netframework-4.8
@@ -32,19 +32,60 @@ namespace Healthcare_System.Models
             }
         }
 
-        public void SendEmailNotification()
+        /// <summary>
+        /// Method to consrtct and send an email containing the emergency alarm notification for a patient
+        /// </summary>
+        /// <returns>boolean inidicating if the email has been sent</returns>
+        public bool SendEmailNotification()
         {
-            
+            bool emailSent = false;
+
+            if (!(EmailMessage == null || ReceipientEmailAddress == null))
+            {
+                ConfigureSMTPClient(); //sets up the smtp client to send the email
+
+                //create the email using the MailMessage class = sets all email properties; inc. to/from email, priority, subject and body
+                MailMessage email = new MailMessage();
+                email.From = new MailAddress("notifications@angliahealthcare.com");
+                email.To.Add(new MailAddress(ReceipientEmailAddress)); //have to add email to the collection structure
+                email.Priority = MailPriority.High; //sets the email priority as high for the alarm
+                email.Subject = "Emergency Alarm Notification";
+                email.Body = EmailMessage;
+
+                //attempt to send the email, within try catch incase of network disconnect
+                try
+                {
+                    smtpClient.Send(email);
+                    emailSent = true;
+                }
+                catch { }
+
+                //release resources consumed by these objects
+                email.Dispose();
+                smtpClient.Dispose();
+
+                //reset the properties ready for new message
+                EmailMessage = null;
+                ReceipientEmailAddress = null;
+            }
+
+            return emailSent;
         }
 
+        /// <summary>
+        /// Method to configure the SMTP client to the secure smtp port 587
+        /// </summary>
         private void ConfigureSMTPClient()
         {
+            //create smtp client to handle sending emails
             smtpClient = new SmtpClient();
+
+            //configuration of smtp properties to use secure port and encrypted communication wih host server
             smtpClient.Port = 587;
             smtpClient.EnableSsl = true;
             smtpClient.Host = "smtp.angliahealthcare.com";
             smtpClient.Timeout = 100;
-            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network; //uses the network to send the email
         }
     }
 }
