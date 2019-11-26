bf355750b114d9e3f918fcbb5b4fa1f7a8eb9359
AD1083 <56340314+AD1083@users.noreply.github.com>
2019-11-26 19:46:49 +0000

    Group work 26/11/19
    @nurbolatbalgat  @uniquedrop  @saltomir

---
+++ Healthcare-System-master/Healthcare_Systemnow/Healthcare_Systemnow/Healthcare_System/Views/PatientModuleView.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Healthcare_System.Views
+{
+    public partial class PatientModuleView : Form, IPatientModuleView
+    {
+
+        public PatientModuleView()
+        {
+            InitializeComponent();
+            this.Load += (sender, args) => Invoke(LoadPatientData);
+
+            btnSetPulseRate.Click += (sender, args) => Invoke(SetPulseRate);
+            btnSetBreathingRate.Click += (sender, args) => Invoke(SetBreathingRate);
+            btnSetBloodPressure.Click += (sender, args) => Invoke(SetBloodPressure);
+            btnSetTemperature.Click += (sender, args) => Invoke(SetTemperature);
+
+            btnRectify.Hide();
+            btnRectify.Click += (sender, args) => Invoke(RectifyAlarm);
+
+            btnBack.Click += BtnBack_Click;
+
+        }
+        // properties
+        public Button BtnRectify { get { return btnRectify; } }
+        public string FirstName { get { return lblFirstName.Text; } set { lblFirstName.Text = value; } }
+        public string LastName { get { return lblLastName.Text; } set { lblLastName.Text = value; } }
+        public string DOB { get { return lblDOB.Text; } set { lblDOB.Text = value; } }
+        public string Condition { get { return lblCondition.Text; } set { lblCondition.Text = value; } }
+        public PictureBox PictureBox { get { return pictureBox; } set { pictureBox = value; } }
+        public string LowerPulseRate { get { return txtboxLowBoundaryPulseRate.Text; } set { txtboxLowBoundaryPulseRate.Text = value; } }
+        public string UpperPulseRate { get { return txtxboxUpperBoundaryPulseRate.Text; } set { txtxboxUpperBoundaryPulseRate.Text = value; } }
+        public string LowerBreathingRate { get { return txtBoxLowBoundaryBreathingRate.Text; } set { txtBoxLowBoundaryBreathingRate.Text = value; } }
+        public string UpperBreathingRate { get { return txtboxUpperBoundaryBreathingRate.Text; } set { txtboxUpperBoundaryBreathingRate.Text = value; } }
+        public string LowerBloodPressure { get { return txtboxLowBoundaryBloodPressure.Text; } set { txtboxLowBoundaryBloodPressure.Text = value; } }
+        public string UpperBloodPressure { get { return txtboxUpperboundaryBloodPressure.Text; } set { txtboxUpperboundaryBloodPressure.Text = value; } }
+        public string LowerTemperature { get { return txtboxLowBoundaryTemperature.Text; } set { txtboxLowBoundaryTemperature.Text = value; } }
+        public string UpperTemperature { get { return txtboxUpperBoundaryTemperture.Text; } set { txtboxUpperBoundaryTemperture.Text = value; } }
+        public string CurPulseRate { get { return lblCurPulseRate.Text; } set { lblCurPulseRate.Text = value; } }
+        public string CurBreathingRate { get { return lblCurBreathingRate.Text; } set { lblCurBreathingRate.Text = value; } }
+        public string CurBloodPressureRate { get { return lblCurBloodPressure.Text; } set { lblCurBloodPressure.Text = value; } }
+        public string CurTemperature { get { return lblCurTemperature.Text; } set { lblCurTemperature.Text = value; } }
+        public string AlarmMessage { get { return lblAlarmMessage.Text; } set { lblAlarmMessage.Text = value; } }
+
+        //Actions
+        public event Action SetPulseRate;
+        public event Action SetBreathingRate;
+        public event Action SetBloodPressure;
+        public event Action SetTemperature;
+        public event Action RectifyAlarm;
+        public event Action LoadPatientData;
+        public event EventHandler GoBack;
+        public event EventHandler UpdatePatientModuleData;
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            if (GoBack != null) GoBack(this, EventArgs.Empty);
+        }
+
+        private void Invoke(Action action)
+        {
+            if (action != null) action();
+        }
+
+        public new void Show()
+        {
+            this.ShowDialog();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (UpdatePatientModuleData != null) UpdatePatientModuleData(this, EventArgs.Empty);
+        }
+
+        public void StopTimer()
+        {
+            timer.Stop();
+        }
+
+        public void RestartTimer()
+        {
+            timer.Start();
+        }
+
+    }
+}
--- Healthcare_Systemnow/Healthcare_Systemnow/SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-﻿update Patient set [PatientPhoto] =
-(select MyImage.* from Openrowset(Bulk 'C:\Users\Aydar\Documents\GitHub\Healthcare-System\Healthcare_Systemnow\Healthcare_Systemnow\Healthcare_System\bin\Debug\Images\waylon.png', Single_Blob) MyImage)
-where PatientID = 1
-
-
